31297c3343a04b2876dc74520e2638086377c750
Pramod <pramodraina047@gmail.com>
2022-11-04 12:26:30 +0000

    Added API for user login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
@@ -38,19 +39,45 @@ app.post("/register/", async (request, response) => {
     response.status = 400;
     response.send("User already exists");
   } else {
-    const createUserQuery = `
-        INSERT INTO 
-          user(username, password, name, gender)
-        VALUES (
-            '${username}',
-            '${hashedPassword}',
-            '${name}',
-            '${gender}'
-        );
+    if (password.length < 5) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      const createUserQuery = `
+            INSERT INTO 
+            user(username, password, name, gender)
+            VALUES (
+                '${username}',
+                '${hashedPassword}',
+                '${name}',
+                '${gender}'
+            );
       `;
-    const dbResponse = await db.run(createUserQuery);
-    const userId = dbResponse.lastID;
-    //console.log(userId);
-    response.send("User created successfully");
+      const dbResponse = await db.run(createUserQuery);
+      const userId = dbResponse.lastID;
+      //console.log(userId);
+      response.send("User created successfully");
+    }
+  }
+});
+
+//User Login API
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched) {
+      const payload = { username: username };
+      const jwtToken = jwt.sign(payload, "SECRET_TOKEN");
+      response.send({ jwtToken: jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
   }
 });
