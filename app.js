5efdccb0b1d8502ba7badb372283d3778753fb4a
Pramod <pramodraina047@gmail.com>
2022-11-04 11:50:34 +0000

    Added API for user registration

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
@@ -10,9 +11,9 @@ const dbPath = path.join(__dirname, "twitterClone.db");
 
 let db = null;
 
-const initializeDBAndServer = () => {
+const initializeDBAndServer = async () => {
   try {
-    db = open({
+    db = await open({
       filename: dbPath,
       driver: sqlite3.Database,
     });
@@ -26,3 +27,30 @@ const initializeDBAndServer = () => {
 };
 
 initializeDBAndServer();
+
+//User Register API
+app.post("/register/", async (request, response) => {
+  const { username, password, name, gender } = request.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+  if (dbUser !== undefined) {
+    response.status = 400;
+    response.send("User already exists");
+  } else {
+    const createUserQuery = `
+        INSERT INTO 
+          user(username, password, name, gender)
+        VALUES (
+            '${username}',
+            '${hashedPassword}',
+            '${name}',
+            '${gender}'
+        );
+      `;
+    const dbResponse = await db.run(createUserQuery);
+    const userId = dbResponse.lastID;
+    //console.log(userId);
+    response.send("User created successfully");
+  }
+});
